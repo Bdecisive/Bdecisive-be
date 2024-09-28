62b8baff4c113e586d06b43355337a0a120d81f3
Limon <limon.nawaj@gmail.com>
2024-09-28 17:30:10 -0500

    first commit

---
+++ src/test/java/edu/ilstu/bdecisive/BdecisiveApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ilstu.bdecisive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdecisiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
