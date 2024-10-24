5b578601f472a1b850d35e4fb82cdfaf27d17aed
jaewoo-kang <jw.kang@cmckorea.org>
2024-10-23 22:00:56 -0500

    Manage Profile

--- src/main/java/edu/ilstu/bdecisive/controllers/UserController.java
+++ src/main/java/edu/ilstu/bdecisive/controllers/UserController.java
@@ -1,15 +1,20 @@
 package edu.ilstu.bdecisive.controllers;
 
+import edu.ilstu.bdecisive.dtos.UserDTO;
 import edu.ilstu.bdecisive.models.User;
 import edu.ilstu.bdecisive.security.response.UserInfoResponse;
 import edu.ilstu.bdecisive.services.UserService;
+import edu.ilstu.bdecisive.utils.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -56,5 +61,18 @@ public class UserController {
     @GetMapping("username")
     public String currentUserName(@AuthenticationPrincipal UserDetails userDetails) {
         return (userDetails != null) ? userDetails.getUsername() : "";
+    }
+
+    @PutMapping("/profile/update")
+    public ResponseEntity<String> updateProfile(@RequestBody UserDTO userProfileDTO, @AuthenticationPrincipal UserDetails userDetails) {
+        try {
+            Optional<User> userOpt = userService.findByUsername(userDetails.getUsername());
+            User user = userOpt.orElseThrow(() -> new RuntimeException("Cannot find the user."));
+
+            userService.updateUserProfile(user.getUserId(), userProfileDTO);
+            return ResponseEntity.ok("Profile updated successfully.");
+        } catch (ServiceException e) {
+            return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+        }
     }
 }
